1250c9a2eacc8ce00f3487628a8bde83c7146007
DedeSnrwn13 <sunarwandede13@gmail.com>
2024-02-03 15:44:25 +0700

    feat: feedback resource

---
+++ app/Filament/Resources/FeedbackResource.php
@@ -0,0 +1,84 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\FeedbackResource\Pages;
+use App\Filament\Resources\FeedbackResource\RelationManagers;
+use App\Models\Feedback;
+use Filament\Forms;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Columns\TextColumn;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class FeedbackResource extends Resource
+{
+    protected static ?string $model = Feedback::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-star';
+
+    protected static ?string $navigationLabel = 'Feedback';
+
+    protected static ?string $navigationGroup = 'Browse Feedback';
+
+    protected static ?int $navigationSort = 1;
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                //
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                TextColumn::make('user.name')
+                    ->sortable()
+                    ->searchable(),
+                TextColumn::make('body')
+                    ->label('message')
+                    ->limit(100)
+                    ->sortable()
+                    ->searchable(),
+                TextColumn::make('rate')
+                    ->numeric()
+                    ->sortable()
+                    ->searchable(),
+                TextColumn::make('created_at')
+                    ->dateTime()
+                    ->sortable()
+                    ->searchable()
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\ViewAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListFeedback::route('/'),
+        ];
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,12 +1,14 @@
 <?php
 
-use App\Http\Controllers\Api\GroundController;
-use App\Http\Controllers\Api\GroundReviewController;
-use App\Http\Controllers\Api\PaymentProviderController;
-use App\Http\Controllers\Api\SportArenaController;
-use App\Http\Controllers\Api\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\UserController;
+use App\Http\Controllers\Api\GroundController;
+use App\Http\Controllers\Api\BookingController;
+use App\Http\Controllers\Api\WebhookController;
+use App\Http\Controllers\Api\SportArenaController;
+use App\Http\Controllers\Api\GroundReviewController;
+use App\Http\Controllers\Api\PaymentProviderController;
 
 /*
 |--------------------------------------------------------------------------
@@ -22,36 +24,54 @@ use Illuminate\Support\Facades\Route;
 Route::group([
     'middleware' => 'api',
     'prefix' => 'auth'
-], function ($router) {
-
-    // User
+], function () {
+    // Guest
     Route::post('/login', [UserController::class, 'login']);
     Route::post('/register', [UserController::class, 'register']);
-    Route::post('/logout', [UserController::class, 'logout']);
     Route::post('/refresh', [UserController::class, 'refresh']);
-    Route::get('/user-profile', [UserController::class, 'userProfile']);
 
-    // Sport Arena
-    Route::prefix('sport-arenas')->name('sport_arenas.')->group(function () {
-        Route::get('/random', [SportArenaController::class, 'random'])->name('random');
-        Route::get('/search/{query}', [SportArenaController::class, 'search'])->name('search');
-        Route::get('{sport_arena}/reviews', [GroundReviewController::class, 'bySportArena'])->name('reviews');
+    Route::middleware('auth:api')->group(function () {
+        // User
+        Route::get('/user-profile', [UserController::class, 'userProfile']);
+        Route::post('/logout', [UserController::class, 'logout']);
 
-        // Ground
-        Route::prefix('{sport_arena}/grounds')->name('grounds.')->group(function () {
-            Route::get('/', [GroundController::class, 'index'])->name('index');
-            Route::get('/search/{query}', [GroundController::class, 'search'])->name('search');
+        // Sport Arena
+        Route::prefix('sport-arenas')->name('sport_arenas.')->group(function () {
+            Route::get('/random', [SportArenaController::class, 'random'])->name('random');
+            Route::get('/search/{query}', [SportArenaController::class, 'search'])->name('search');
+            Route::get('/{sport_arena}/reviews', [GroundReviewController::class, 'bySportArena'])->name('reviews');
 
-            // Review
-            Route::prefix('{ground}/reviews')->name('reviews.')->group(function () {
-                Route::get('/', [GroundReviewController::class, 'byGround'])->name('ground');
+            // Ground
+            Route::prefix('{sport_arena}/grounds')->name('grounds.')->group(function () {
+                Route::get('/', [GroundController::class, 'index'])->name('index');
+                Route::get('/search/{query}', [GroundController::class, 'search'])->name('search');
+
+                // Review
+                Route::prefix('{ground}/reviews')->name('reviews.')->group(function () {
+                    Route::get('/', [GroundReviewController::class, 'byGround'])->name('ground');
+                });
             });
         });
+
+        // Review
+        Route::get('reviews/random', [GroundReviewController::class, 'random'])->name('reviews');
+
+        // Payment Provider
+        Route::get('payment-provider', [PaymentProviderController::class, 'index'])->name('payment-provider');
+
+        // Booking
+        Route::prefix('bookings')->name('bookings.')->group(function () {
+            Route::post('/', [BookingController::class, 'booking'])->name('store');
+            Route::get('/history', [BookingController::class, 'history'])->name('history');
+            Route::get('/{id}/show', [BookingController::class, 'show'])->name('show');
+            Route::put('/{id}/cancel', [BookingController::class, 'cancel'])->name('cancel');
+        });
+
+        // Feedback
+        Route::post('/feedback', [UserController::class, 'feedback'])->name('feedback');
     });
 
-    // Review
-    Route::get('reviews/random', [GroundReviewController::class, 'random'])->name('reviews');
 
-    // Payment Provider
-    Route::get('payment-provider', [PaymentProviderController::class, 'index'])->name('payment-provider');
 });
+
+Route::post('/webhook/midtrans', [WebhookController::class, 'midtransCallback'])->name('webhook.midtrans');
