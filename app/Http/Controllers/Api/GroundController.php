26a1a9cbf343fb6292b356ce7f28a20c9d564e20
DedeSnrwn13 <sunarwandede13@gmail.com>
2024-02-04 08:30:41 +0700

    feat: index & show ground api

--- app/Http/Controllers/Api/GroundController.php
+++ app/Http/Controllers/Api/GroundController.php
@@ -2,17 +2,61 @@
 
 namespace App\Http\Controllers\Api;
 
-use App\Http\Controllers\Controller;
+use App\Models\Ground;
 use App\Models\SportArena;
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
 
 class GroundController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware('auth:api', ['except' => ['login', 'register']]);
+    }
+
     public function index(SportArena $sportArena)
     {
-        $grounds = $sportArena->grounds;
+        $grounds =  Ground::with(['sportArena', 'groundReviews' => function ($query) {
+            $query->selectRaw('AVG(rate) as average_rate, ground_id')
+                ->groupBy('ground_id');
+        }])
+        ->where('sport_arena_id', $sportArena->id)
+        ->where('is_available', true)
+        ->get();
 
-        return response()->json(['data' => $grounds]);
+        $result = $grounds->map(function ($ground) {
+            $groundWithImage = $ground;
+
+            $image = null;
+            if (is_array($groundWithImage->image) && count($groundWithImage->image) > 0) {
+                $image = $groundWithImage->image[0];
+            }
+            $rate = $groundWithImage->groundReviews()->avg('rate');
+            $price = $groundWithImage->avg('rental_price');
+
+            $groundData = $ground->toArray();
+            $groundData['rate'] = $rate;
+            $groundData['price'] = $price;
+            $groundData['image'] = $image;
+            $groundData['image_thumbnail'] = $image;
+
+            return $groundData;
+        });
+
+        return response()->json(['data' => $result]);
+    }
+
+    public function show(SportArena $sportArena, Ground $ground)
+    {
+        $ground = $ground->with('sportArena')->firstOrFail();
+        $rate = $ground->groundReviews()->avg('rate');
+
+        $groundData = $ground->toArray();
+        $groundData['rate'] = $rate;
+
+        return response()->json([
+            'data' => $groundData
+        ]);
     }
 
     public function search(SportArena $sportArena, $query)
